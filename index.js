9a9e41f3b2a5732c14ef2d50f8b83ac222fbbc71
josiah <devs.josia@gmail.com>
2022-11-29 10:21:50 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  let matched = [];
+  for (const person of drivers) {
+    if (person.toLowerCase() === name.toLowerCase()) {
+      matched.push(person);
+    }
+  }
+  return matched;
+}
+
+function fuzzyMatch(drivers, name) {
+  let matched = [];
+  for (const person of drivers) {
+    if (person.slice(0, 2).toLowerCase() === name.slice(0, 2).toLowerCase()) {
+      matched.push(person);
+    }
+  }
+  return matched;
+}
+
+function matchName(driver, name) {
+  let matched = [];
+  for (const person of driver) {
+    if (person.name === name) {
+      matched.push(person);
+    }
+  }
+  return matched;
+}
